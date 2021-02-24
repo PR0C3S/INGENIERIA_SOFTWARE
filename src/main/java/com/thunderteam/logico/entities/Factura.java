c8526f52c29101496258a6cb795d3736cce7890a
Robert Dominguez <robertdominguez7@gmail.com>
2021-02-24 17:03:59 -0400

    ADD Factura

---
+++ src/main/java/com/thunderteam/logico/entities/Factura.java
@@ -0,0 +1,12 @@
+package com.thunderteam.mvcdemo.entities;
+
+import java.util.Date;
+import com.thunderteam.logico.*;
+
+public class Factura {
+
+    private int ID_Factura;
+    private Banco contrato;
+    private Date fecha;
+    private float pago;
+}
